2e7b119fba3f14b72c134375c421ce8ee934a484
yunseok.ji <yunseok.j@gmail.com>
2018-03-30 18:36:25 +0900

    9 Back-end api test

---
+++ routes/api/data/company/ctrls.js
@@ -0,0 +1,15 @@
+exports.list = (req, res) => {
+  res.send({ success: false, msg: 'list 준비중입니다' });
+};
+
+exports.add = (req, res) => {
+  res.send({ success: false, msg: 'add 준비중입니다' });
+};
+
+exports.mod = (req, res) => {
+  res.send({ success: false, msg: 'mod 준비중입니다' });
+};
+
+exports.del = (req, res) => {
+  res.send({ success: false, msg: 'del 준비중입니다' });
+};
